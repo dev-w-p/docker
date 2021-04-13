1667b334897c6246aea6418600f4c1f0013d8a78
dev-w-p <dev.y.taka@gmail.com>
2021-04-14 06:13:24 +0900

    コミット

--- server.py
+++ server.py
@@ -3,5 +3,5 @@ PORT = int(os.environ['PORT'])
 app = flask.Flask('app server')
 @app.route('/')
 def index():
-    return 'hello Dockerfile'
+    return 'Hello Dockerfile'
 app.run(debug=True, host='0.0.0.0', port=PORT)
